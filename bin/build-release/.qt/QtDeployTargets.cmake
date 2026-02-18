757bd4f483397146873eccb74e640578e3c28bba
wzlyy <137384203+wzlyy@users.noreply.github.com>
2026-02-18 12:04:51 +0700

    Delete bin/build-release directory

--- bin/build-release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Tucil1_FILE C:/Users/wilda/GitHub/Tucil1_13524054/bin/build-release/Tucil1.exe)
-set(__QT_DEPLOY_TARGET_Tucil1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Tucil1_RUNTIME_DLLS C:/Users/wilda/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Users/wilda/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Users/wilda/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
